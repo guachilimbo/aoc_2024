5dd96b7e5c0e15a309a6c7cb04ac02f2b8aff3f2
guachilimbo <ignacioballesterosruiz@gmail.com>
2024-12-06 16:37:47 +0000

    day3 part2

--- day3.py
+++ day3.py
@@ -12,8 +12,23 @@ def get_total(memory):
     for a, b in get_muls(memory):
         res += int(a) * int(b)
     return res
+
+
+
+def get_total2(memory):
+    res = 0
+    multiplications = re.findall(r"(?s)don't\(\).*?(?:do\(\)|$)|mul\((\d+),(\d+)\)", memory)
+    for a, b in multiplications:
+        if a and b:
+            res += int(a) * int(b)
+    return res
+
     
 if __name__ == "__main__":
     assert get_total("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))") == 161
+    assert get_total2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))") == 48
+
     memory = parse_input("./day3_input.txt")
     print(get_total(memory))
+    print(get_total2(memory))
+    
